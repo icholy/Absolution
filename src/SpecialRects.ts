2e5892521380da17a6154411f3ca3ba560ad9bd3
Ilia Choly <icholy@accipiterradar.com>
2016-08-21 00:00:00 -0400

    add body rect

---
+++ src/SpecialRects.ts
@@ -0,0 +1,73 @@
+
+module Robin {
+
+  export class DocumentRect extends Rect {
+
+    constructor(layout: Layout) {
+      super(layout, "document");
+      window.addEventListener("resize", () => {
+        this.updateSystemPosition();
+        layout.update();
+      });
+      this.updateSystemPosition();
+    }
+
+    private updateSystemPosition(): void {
+      let element = document.documentElement;
+      let position = Utils.getRectPosition(element);
+      this.left.assignValue(position.left);
+      this.top.assignValue(position.top);
+      this.width.assignValue(position.width);
+      this.height.assignValue(position.height);
+    }
+
+  }
+
+  export class ViewportRect extends Rect {
+
+    constructor(layout: Layout) {
+      super(layout, "viewport");
+      window.addEventListener("resize", () => {
+        this.updateSystemPosition();
+        this.layout.update();
+      });
+      window.addEventListener("scroll", () => {
+        this.updateSystemPosition();
+        this.layout.update();
+      });
+      this.updateSystemPosition();
+    }
+
+    private updateSystemPosition(): void {
+      let position = Utils.getViewportRectPosition();
+      this.left.assignValue(position.left);
+      this.top.assignValue(position.top);
+      this.width.assignValue(position.width);
+      this.height.assignValue(position.height);
+    }
+
+  }
+
+  export class BodyRect extends Rect {
+
+    constructor(layout: Layout) {
+      super(layout, "body");
+      window.addEventListener("resize", () => {
+        this.updateSystemPosition();
+        layout.update();
+      });
+      this.updateSystemPosition();
+    }
+
+    private updateSystemPosition(): void {
+      let element = document.body;
+      let position = Utils.getRectPosition(element);
+      this.left.assignValue(position.left);
+      this.top.assignValue(position.top);
+      this.width.assignValue(position.width);
+      this.height.assignValue(position.height);
+    }
+
+  }
+
+}
