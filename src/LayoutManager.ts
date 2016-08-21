52306fd9a4a6db76960579588ff6bff405dd9689
Ilia Choly <ilia.choly@gmail.com>
2016-08-21 00:00:00 -0400

    add some comments

--- src/LayoutManager.ts
+++ src/LayoutManager.ts
@@ -9,18 +9,27 @@ class LayoutManager {
   system = new System();
   managers = [] as ElementManager[];
 
+  /**
+   * Register an element with the layout
+   */
   register(element: HTMLElement, container?: HTMLElement): ElementManager {
     let m = new ElementManager(this.system, element, container);
     this.managers.push(m);
     return m;
   }
 
+  /**
+   * Update the system from the element's actual values
+   */
   updateSystem(): void {
     for (let manager of this.managers) {
       manager.updateSystem();
     }
   }
 
+  /**
+   * Update the elements with the system's value
+   */
   updateLayout(): void {
     this.system.solve();
     for (let manager of this.managers) {
