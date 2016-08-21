b3d1371310290fc367045f1b53856f8abf745fc8
Ilia Choly <icholy@accipiterradar.com>
2016-08-21 00:00:00 -0400

    use pre & post functions in directive

--- src/Angular.ts
+++ src/Angular.ts
@@ -58,16 +58,42 @@ module Robin.Angular {
     }
   }
 
+  class Controller {
+    private options: RectOptions;
+
+    setOptions(options: RectOptions): void {
+      this.options = options;
+    }
+
+    getOptionsWithContext(context: ScopeContext): RectOptions {
+      return {
+        id:        this.options.id,
+        container: this.options.container,
+        watcher:   this.options.watcher,
+        rules:     this.options.rules,
+        context:   context
+      };
+    }
+  }
+
   function Directive(layout: Layout): ng.IDirective {
     return {
       restrict: "A",
       scope: false,
-      link: (scope: ng.IScope, element: ng.IAugmentedJQuery, attr: ng.IAttributes) => {
-        let el = element[0];
-        let options = layout.getRectOptions(el, true);
-        options.context = new ScopeContext(layout, scope);
-        let rect = new ElementRect(el, layout, options);
-        element.on("$destroy", () => rect.destroy());
+      controller: Controller,
+      link: {
+        pre(scope: ng.IScope, element: ng.IAugmentedJQuery, attr: ng.IAttributes, ctrl: Controller): void {
+          let el = element[0];
+          let options = layout.getRectOptions(el, true);
+          ctrl.setOptions(options);
+        },
+        post(scope: ng.IScope, element: ng.IAugmentedJQuery, attr: ng.IAttributes, ctrl: Controller): void {
+          let el = element[0];
+          let context = new ScopeContext(layout, scope);
+          let options = ctrl.getOptionsWithContext(context)
+          let rect = new ElementRect(el, layout, options);
+          element.on("$destroy", () => rect.destroy());
+        }
       }
     }
   }
