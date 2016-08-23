2d01c8ea61d9c6395ecf2d04038384c23fae0d89
Ilia Choly <ilia.choly@gmail.com>
2016-08-22 23:13:22 -0400

    allow using  to reference self

--- src/Environment.ts
+++ src/Environment.ts
@@ -7,21 +7,41 @@ module uzi {
 
   export class RectContext extends Context {
 
+    private id:        string;
     private container: string;
 
     constructor(options: RectOptions) {
       super();
+      this.id        = options.id;
       this.container = options.container;
     }
 
-    identToName(node: IdentNode): string {
-      if (node.tag === "ident" && node.value === "parent") {
+    private lookupIdent(node: IdentNode): string {
+      if (node.value === "parent" && this.container) {
         return this.container;
       }
-      if (node.tag === "property" && node.object === "parent") {
-        return `${this.container}.${node.key}`;
+      if (node.value === "this") {
+        return this.id;
       }
       return node.value;
+    }
+
+    private lookupProperty(node: IdentNode): string {
+      if (node.object === "parent" && this.container) {
+        return `${this.container}.${node.key}`;
+      }
+      if (node.object === "this") {
+        return `${this.id}.${node.key}`;
+      }
+      return node.value;
+    }
+
+    identToName(node: IdentNode): string {
+      if (node.tag === "ident") {
+        return this.lookupIdent(node);
+      } else {
+        return this.lookupProperty(node);
+      }
     }
 
   }
