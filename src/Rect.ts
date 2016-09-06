0b028c98bc26e9e79ac12999a3cb997eff1833ad
Ilia Choly <icholy@accipiterradar.com>
2016-09-06 16:37:28 -0400

    destroy a rects relationships instead of its variables

--- src/Rect.ts
+++ src/Rect.ts
@@ -6,6 +6,7 @@ module uzi {
   export abstract class Rect {
 
     protected system: System;
+    private relationships: RelationshipGroup;
 
     // these variables represent the rect's properties
     // inside the constaint system
@@ -22,31 +23,35 @@ module uzi {
       protected container?: string
     ) {
 
-      let system = this.system = engine.getSystem();
+      let system = engine.getSystem();
+      this.system = system;
+
+      let relationships = new RelationshipGroup(system);
+      this.relationships = relationships;
 
       // x axis
-      system.subtract(`${id}.width`, `${id}.right`, `${id}.left`);
-      system.divide(`${id}_tmp1`, `${id}.width`, 2);
-      system.add(`${id}.center-x`, `${id}.left`, `${id}_tmp1`);
+      relationships.subtract(`${id}.width`, `${id}.right`, `${id}.left`);
+      relationships.divide(`${id}_tmp1`, `${id}.width`, 2);
+      relationships.add(`${id}.center-x`, `${id}.left`, `${id}_tmp1`);
 
       this.left  = system.getVariable(`${id}.left`);
       this.width = system.getVariable(`${id}.width`);
 
       // y axis
-      system.subtract(`${id}.height`, `${id}.bottom`, `${id}.top`);
-      system.divide(`${id}_tmp2`, `${id}.height`, 2);
-      system.add(`${id}.center-y`, `${id}.top`, `${id}_tmp2`);
+      relationships.subtract(`${id}.height`, `${id}.bottom`, `${id}.top`);
+      relationships.divide(`${id}_tmp2`, `${id}.height`, 2);
+      relationships.add(`${id}.center-y`, `${id}.top`, `${id}_tmp2`);
 
       this.top    = system.getVariable(`${id}.top`);
       this.height = system.getVariable(`${id}.height`);
 
       // container offsets
       if (container) {
-        system.subtract(`${id}.left-offset`, `${id}.left`, `${container}.left`);
-        system.subtract(`${id}.top-offset`, `${id}.top`, `${container}.top`);
+        relationships.subtract(`${id}.left-offset`, `${id}.left`, `${container}.left`);
+        relationships.subtract(`${id}.top-offset`, `${id}.top`, `${container}.top`);
       } else {
-        system.equals(`${id}.left-offset`, `${id}.left`);
-        system.equals(`${id}.top-offset`, `${id}.top`);
+        relationships.equals(`${id}.left-offset`, `${id}.left`);
+        relationships.equals(`${id}.top-offset`, `${id}.top`);
       }
 
       this.topOffset = system.getVariable(`${id}.top-offset`);
@@ -57,13 +62,7 @@ module uzi {
      * Destroy the Rect and all its variables.
      */
     destroy(): void {
-      let system = this.system;
-      system.destroyVariable(this.top);
-      system.destroyVariable(this.topOffset);
-      system.destroyVariable(this.height);
-      system.destroyVariable(this.left);
-      system.destroyVariable(this.leftOffset);
-      system.destroyVariable(this.width);
+      this.relationships.destroy();
     }
 
     getId(): string {
